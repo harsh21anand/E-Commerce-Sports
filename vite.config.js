26c6ee395e2b1366c6c2b7a36173fa2f77cd3097
harsh <harshanand160802@gmail.com>
2025-07-29 03:13:58 +0530

    Merge remote changes

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-<<<<<<< HEAD
  
-=======
- base: "/E-Commerce-Sports/",
->>>>>>> f7643d696d738bdfd082a8d5227d3dfa371487dd
   plugins: [react(),
     tailwindcss()
   ],
